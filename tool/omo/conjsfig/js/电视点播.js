7f069d7238054cf90084ff0428198d6aa28c92f1
chichi <chichi@noreply.codeberg.org>
2024-04-16 02:03:56 +0000

    上传文件至 tool/omo/conjsfig/js

---
+++ tool/omo/conjsfig/js/电视点播.js
@@ -0,0 +1,18 @@
+[
+{"name": "闲鱼资源","url":"https://test.fuxiashenzuoshi.top/20231014fweyev"},
+{"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt"},
+{"name": "一木线路",     "url": "http://home.jundie.top:81/ray/tvlive"},
+{"name": "多多影音","url": "https://bitbucket.org/xduo/duoapi/raw/master/v.txt&&&https://myhkw.cn/open/img/mm"},
+{"name": "Ray0直播","url": "https://xhdwc.tk/tvlive.txt&&&https://s1.ax1x.com/2023/06/08/pCkdDKg.jpg"},
+{"name": "侯用心","url":"https://agit.ai/houyongxin/monkey/raw/branch/master/tv/live22.txt"},
+{"name":"观园","url":"https://www.notabug.org/keke9088/TVBOX/raw/master/大观园.txt"},
+{"name":"月夕","url":"https://codeberg.org/Calendre/TVBox/raw/branch/main/Live/live.txt"},
+{"name":"秋语","url":"https://agit.ai/767820774/gao/raw/branch/master/list.txt"},
+{"name":"ihymn","url":"https://agit.ai/ihymn/tvbox/raw/branch/master/tv/zblive.txt"},
+{"name": "月光宝盒",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/tvlive.txt"},
+{"name": "BESTVm3u8",     "url": "https://telegram-feiyangdigital.v1.mk/bestv.m3u"},
+{"name": "霜辉月明",     "url": "http://tv.jthn.cn/lives/live_plus.txt"},
+{"name": "老刘备",     "url": "http://9xi4o.tk/sub/mytv.txt"},
+{"name": "南风线路",     "url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/live.txt"},
+{"name": "影视",     "url": "https://agit.ai/gk0747/1/raw/branch/master/zb.txt&&&https://agit.ai/kuku/kot/raw/branch/master/js/icon_img_placeholder6.png"}
+]
